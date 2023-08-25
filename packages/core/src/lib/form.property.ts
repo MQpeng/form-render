4509fe321865c4294f5c3714b2786f9d3eb854e8
Bean <pengshaobin@deepexi.com>
2023-08-25 17:10:15 +0800

    feat: form property

---
+++ packages/core/src/lib/form.property.ts
@@ -0,0 +1,5 @@
+export const SF_SEQ = '/';
+
+export abstract class FormProperty {}
+
+export abstract class PropertyGroup extends FormProperty {}
--- packages/core/src/utils/round.spec.ts
+++ packages/core/src/utils/round.spec.ts
@@ -1,13 +1,16 @@
-import { NzSafeAny } from 'ng-zorro-antd/core/types';
-
 import * as ROUND from './round';
 
 describe('util.round', () => {
-  function run(methodName: keyof typeof ROUND, data: Array<{ k: NzSafeAny[]; v: number }>): void {
+  function run(
+    methodName: keyof typeof ROUND,
+    data: Array<{ k: any[]; v: number }>
+  ): void {
     for (const item of data) {
       const value = item.k[0];
       const precision = item.k.length === 2 ? item.k[1] : undefined;
-      expect(ROUND[methodName](value, precision)).withContext(`${item.k} must be ${item.v}`).toBe(item.v);
+      expect(ROUND[methodName](value, precision))
+        // .withContext(`${item.k} must be ${item.v}`)
+        .toBe(item.v);
     }
   }
 
@@ -15,7 +18,7 @@ describe('util.round', () => {
     const data: Array<{ k: unknown[]; v: number }> = [
       { k: [4.006], v: 5 },
       { k: [6.004, 2], v: 6.01 },
-      { k: [6040, -2], v: 6100 }
+      { k: [6040, -2], v: 6100 },
     ];
     run('ceil', data);
   });
@@ -24,7 +27,7 @@ describe('util.round', () => {
     const data: Array<{ k: unknown[]; v: number }> = [
       { k: [4.006], v: 4 },
       { k: [0.046, 2], v: 0.04 },
-      { k: [4060, -2], v: 4000 }
+      { k: [4060, -2], v: 4000 },
     ];
     run('floor', data);
   });
@@ -33,7 +36,7 @@ describe('util.round', () => {
     const data: Array<{ k: unknown[]; v: number }> = [
       { k: [4.006], v: 4 },
       { k: [4.006, 2], v: 4.01 },
-      { k: [4060, -2], v: 4100 }
+      { k: [4060, -2], v: 4100 },
     ];
     run('round', data);
   });
